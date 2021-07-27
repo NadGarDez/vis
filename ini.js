42e8990ca4ab0afa9fe268400e2d0feae7ec2962
NadGarDez <iranad@hotmail.com>
2021-07-27 12:53:21 -0400

    actualizacion de las politicas

--- ini.js
+++ ini.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {StyleSheet, AppRegistry, ScrollView, Image, Text,Alert,View,TouchableOpacity,ActivityIndicator} from 'react-native';
+import {StyleSheet, AppRegistry, ScrollView, Image, Text,Alert,View,TouchableOpacity,ActivityIndicator, Platform} from 'react-native';
+import {requestTrackingPermissionsAsync} from "expo-tracking-transparency";
 import { WebView } from 'react-native-webview';
 
 import E from "./error.js"
+import N from "./noGranted.js"
 
 
 export default class Ini extends Component{
@@ -39,44 +41,175 @@ export default class Ini extends Component{
 	}
 	render(){
 
+		let rec = this.recargar
+
+		console.log(rec)
+
+		let B = async ()=>{
+
+			let conponent = ""
+			/*
+
+			if(Platform.OS == "ios"){
+				/*const { status } = await requestTrackingPermissionsAsync();
+
+				if (status == "granted"){
+
+					if(this.state.e == false){
+						console.log(this.state)
+
+						component = (
+								<WebView source={{ uri: this.state.secciones[this.state.view].src }} style={{ marginTop: 20, width:'100%',height:'90%' }}
+									onError={
+										(e)=>{
+											//this.state.error = true
+											console.log("error ${e}")
+											this.setState({e:true})
+										}
+									}
+									onLoad={(syntheticEvent) => {
+							    const { nativeEvent } = syntheticEvent;
+							    console.log(nativeEvent.url)
+								//	this.setState({webv:"visible"})
+							  }}
+								/>
+						)
 
 
-		let A = ()=>{
+					}
+					else{
+						component=(
+								<E recargar={rec}/>
+						)
+					}
 
-			let component=""
-			if(this.state.e == false){
-				console.log(this.state)
+				}
+				else{
+					component = (
+						<N recargar={rec} />
+					)
+				}
+			}
+			else {/*
+				let component=""
+				if(this.state.e == false){
+					console.log(this.state)
 
-				component = (
-						<WebView source={{ uri: this.state.secciones[this.state.view].src }} style={{ marginTop: 20, width:'100%',height:'90%' }}
-							onError={
-								(e)=>{
-									//this.state.error = true
-									console.log("error ${e}")
-									this.setState({e:true})
+					component = (
+							<WebView source={{ uri: this.state.secciones[this.state.view].src }} style={{ marginTop: 20, width:'100%',height:'90%' }}
+								onError={
+									(e)=>{
+										//this.state.error = true
+										console.log("error ${e}")
+										this.setState({e:true})
+									}
 								}
+								onLoad={(syntheticEvent) => {
+						    const { nativeEvent } = syntheticEvent;
+						    console.log(nativeEvent.url)
+							//	this.setState({webv:"visible"})
+						  }}
+							/>
+					)
+				}
+
+			}*/
+			component = (
+				<N/>
+			)
+
+
+			return component
+
+		}
+
+
+		const A = ()=>{
+			let component = ""
+			if(Platform.OS == "ios"){
+				requestTrackingPermissionsAsync()
+				.then(
+					(res)=>{
+						console.log(res)
+						if(res.status == "granted"){
+							if(this.state.e == false){
+								console.log(this.state)
+
+								component = (
+										<WebView source={{ uri: this.state.secciones[this.state.view].src }} style={{ marginTop: 20, width:'100%',height:'90%' }}
+											onError={
+												(e)=>{
+													//this.state.error = true
+													console.log("error ${e}")
+													this.setState({e:true})
+												}
+											}
+											onLoad={(syntheticEvent) => {
+									    const { nativeEvent } = syntheticEvent;
+									    console.log(nativeEvent.url)
+										//	this.setState({webv:"visible"})
+									  }}
+										/>
+								)
 							}
-							onLoad={(syntheticEvent) => {
-					    const { nativeEvent } = syntheticEvent;
-					    console.log(nativeEvent.url)
-						//	this.setState({webv:"visible"})
-					  }}
-						/>
+							else{
+								component=(
+										<E recargar={rec}/>
+								)
+							}
+						}
+						else{
+							component = (
+								<N recargar={rec} />
+							)
+						}
+					}
 				)
-
-
+				.catch(
+					(err)=>{
+							console.log(res)
+					}
+				)
 			}
 			else{
-				component=(
-						<E recargar={this.recargar}/>
-				)
+				if(this.state.e == false){
+					console.log(this.state)
+
+					component = (
+							<WebView source={{ uri: this.state.secciones[this.state.view].src }} style={{ marginTop: 20, width:'100%',height:'90%' }}
+								onError={
+									(e)=>{
+										//this.state.error = true
+										console.log("error ${e}")
+										this.setState({e:true})
+									}
+								}
+								onLoad={(syntheticEvent) => {
+						    const { nativeEvent } = syntheticEvent;
+						    console.log(nativeEvent.url)
+							//	this.setState({webv:"visible"})
+						  }}
+							/>
+					)
+				}
+				else{
+					component=(
+							<E recargar={rec}/>
+					)
+				}
 			}
+
+
+
+
 			return component
 		}
+
 		return(
 
 			<View style={{width:'100%',height:'100%',padding:0,margin:0}}>
-				<A />
+				<A/>
+
 				<View style={{ width:'100%',height:'10%',backgroundColor:'rgba(52, 52, 52, alpha)',display:'flex',justifyContent:'center',flexDirection:'row'}}>
 
 					<TouchableOpacity  style={{width:'33%',height:'100%'}}
